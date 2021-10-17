7efac9ba922ade77c2f523ff6f5303ce423a0198
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-10-17 18:53:32 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -8,9 +8,12 @@
 import sys
 from importlib import import_module
 
+from pytgcalls import idle
 from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
-
-from userbot import ALIVE_NAME, BOT_VER, LOGS, bot
+from telethon.tl.functions.channels import JoinChannelRequest
+from userbot import ALIVE_NAME, BOT_VER, BOTLOG_CHATID
+from userbot import CMD_HANDLER as cmd
+from userbot import LOGS, UPSTREAM_REPO_BRANCH, bot, call_py
 from userbot.modules import ALL_MODULES
 
 INVALID_PH = (
@@ -20,6 +23,7 @@ INVALID_PH = (
 
 try:
     bot.start()
+    call_py.start()
 except PhoneNumberInvalidError:
     print(INVALID_PH)
     sys.exit(1)
@@ -28,12 +32,33 @@ for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
 LOGS.info(
-    f"Jika {ALIVE_NAME} Membutuhkan Bantuan, Silahkan Gabung ke Grup https://t.me/SharingUserbot")
+    f"Jika {ALIVE_NAME} Membutuhkan Bantuan, Silahkan Gabung ke Grup https://t.me/SharingUserbot"
+)
 
-LOGS.info(
-    f"Man-Userbot ⚙️ V{BOT_VER} [🔥 BERHASIL DIAKTIFKAN! 🔥]")
+LOGS.info(f"Man-Userbot ⚙️ V{BOT_VER} [🔥 BERHASIL DIAKTIFKAN! 🔥]")
 
 
+async def man_userbot_on():
+    try:
+        if BOTLOG_CHATID != 0:
+            await bot.send_message(
+                BOTLOG_CHATID,
+                f"🔥 **Man-Userbot Berhasil Di Aktifkan**\n━━\n➠ **Userbot Version -** `{BOT_VER}@{UPSTREAM_REPO_BRANCH}`\n➠ **Ketik** `{cmd}alive` **untuk Mengecheck Bot**\n━━",
+            )
+    except Exception as e:
+        LOGS.info(str(e))
+    # KALO LU NGEFORK LINK CH & GRUP PUNYA GUA NYA JANGAN DI HAPUS YA GOBLOK 😡
+    try:
+        await bot(JoinChannelRequest("@Lunatic0de"))
+        await bot(JoinChannelRequest("@SharingUserbot"))
+    except BaseException:
+        pass
+
+
+# JANGAN DI HAPUS GOBLOK 😡 LU COPY/EDIT AJA TINGGAL TAMBAHIN PUNYA LU
+# DI HAPUS GUA GBAN YA 🥴 GUA TANDAIN LU AKUN TELENYA 😡
+bot.loop.create_task(man_userbot_on())
+idle()
 if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
 else:
