66e0cefebc911fc473d4be1bc6fb90dd7840a2fd
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-10-26 01:53:12 +0700

    Update tag.py

--- userbot/modules/tag.py
+++ userbot/modules/tag.py
@@ -11,77 +11,65 @@ from telethon.tl.types import UserStatusOnline as onn
 from telethon.tl.types import UserStatusRecently as rec
 from telethon.utils import get_display_name
 
-from userbot import CMD_HELP
-from userbot.events import register
+from userbot import CMD_HANDLER as cmd
+from userbot import CMD_HELP, bot
+from userbot.events import man_cmd
 
 
-@register(
-    outgoing=True,
-    pattern=r"^\.tag(on|off|all|bots|rec|admins|owner)?(.*)",
-    disable_errors=True,
+@bot.on(
+    man_cmd(outgoing=True, pattern="tag(on|off|all|bots|rec|admins|owner)?(.*)")
 )
 async def _(e):
     okk = e.text
     lll = e.pattern_match.group(2)
-    users = 0
     o = 0
     nn = 0
     rece = 0
-    if lll:
-        xx = f"{lll}"
-    else:
-        xx = ""
-    async for bb in e.client.iter_participants(e.chat_id, 99):
-        users = users + 1
+    xx = f"{lll}" if lll else ""
+    xnxx = await e.client.get_participants(e.chat_id, limit=99)
+    for users, bb in enumerate(xnxx):
         x = bb.status
         y = bb.participant
         if isinstance(x, onn):
-            o = o + 1
+            o += 1
             if "on" in okk:
                 xx += f"\n⚜️ [{get_display_name(bb)}](tg://user?id={bb.id})"
         if isinstance(x, off):
-            nn = nn + 1
-            if "off" in okk:
-                if not (bb.bot or bb.deleted):
-                    xx += f"\n⚜️ [{get_display_name(bb)}](tg://user?id={bb.id})"
-        if isinstance(x, rec):
-            rece = rece + 1
-            if "rec" in okk:
-                if not (bb.bot or bb.deleted):
-                    xx += f"\n⚜️ [{get_display_name(bb)}](tg://user?id={bb.id})"
-        if isinstance(y, owner):
-            if "admin" or "owner" in okk:
-                xx += f"\n👑 [{get_display_name(bb)}](tg://user?id={bb.id}) 👑"
-        if isinstance(y, admin):
-            if "admin" in okk:
-                if not bb.deleted:
-                    xx += f"\n⚜️ [{get_display_name(bb)}](tg://user?id={bb.id})"
-        if "all" in okk:
-            if not (bb.bot or bb.deleted):
+            nn += 1
+            if "off" in okk and not bb.bot and not bb.deleted:
                 xx += f"\n⚜️ [{get_display_name(bb)}](tg://user?id={bb.id})"
-        if "bot" in okk:
-            if bb.bot:
-                xx += f"\n🤖 [{get_display_name(bb)}](tg://user?id={bb.id})"
+        if isinstance(x, rec):
+            rece += 1
+            if "rec" in okk and not bb.bot and not bb.deleted:
+                xx += f"\n⚜️ [{get_display_name(bb)}](tg://user?id={bb.id})"
+        if isinstance(y, owner):
+            xx += f"\n👑 [{get_display_name(bb)}](tg://user?id={bb.id}) 👑"
+        if isinstance(y, admin) and "admin" in okk and not bb.deleted:
+            xx += f"\n⚜️ [{get_display_name(bb)}](tg://user?id={bb.id})"
+        if "all" in okk and not bb.bot and not bb.deleted:
+            xx += f"\n⚜️ [{get_display_name(bb)}](tg://user?id={bb.id})"
+        if "bot" in okk and bb.bot:
+            xx += f"\n🤖 [{get_display_name(bb)}](tg://user?id={bb.id})"
     await e.client.send_message(e.chat_id, xx)
     await e.delete()
 
 
 CMD_HELP.update(
     {
-        "tagger": "**Plugin : **`tagger`\
-        \n\n  •  **Syntax :** `.tagall`\
+        "tagger": f"**Plugin : **`tagger`\
+        \n\n  •  **Syntax :** `{cmd}tagall`\
         \n  •  **Function : **Tag Top 100 Members di group chat.\
-        \n\n  •  **Syntax :** `.tagowner`\
+        \n\n  •  **Syntax :** `{cmd}tagowner`\
         \n  •  **Function : **Tag Owner group chat\
-        \n\n  •  **Syntax : **`.tagadmins`\
+        \n\n  •  **Syntax : **`{cmd}tagadmins`\
         \n  •  **Function : **Tag Admins group chat.\
-        \n\n  •  **Syntax :** `.tagbots`\
+        \n\n  •  **Syntax :** `{cmd}tagbots`\
         \n  •  **Function : **Tag Bots group chat.\
-        \n\n  •  **Syntax :** `.tagrec`\
+        \n\n  •  **Syntax :** `{cmd}tagrec`\
         \n  •  **Function : **Tag Member yang Baru Aktif.\
-        \n\n  •  **Syntax :** `.tagon`\
+        \n\n  •  **Syntax :** `{cmd}tagon`\
         \n  •  **Function : **Tag Online Members (hanya berfungsi jika privasi dimatikan)\
-        \n\n  •  **Syntax :** `.tagoff`\
+        \n\n  •  **Syntax :** `{cmd}tagoff`\
         \n  •  **Function : **Tag Offline Members (hanya berfungsi jika privasi dimatikan)\
         "
     }
