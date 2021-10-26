eccd3cb035612ebe1815a3051c752224531b1fdc
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-10-27 06:50:11 +0700

    Update other.py

--- userbot/modules/other.py
+++ userbot/modules/other.py
@@ -13,18 +13,21 @@ from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.messages import ExportChatInviteRequest
 from telethon.tl.types import ChannelParticipantsKicked
+from telethon.utils import get_display_name
 
-from userbot import ALIVE_NAME, CMD_HELP, bot
-from userbot.events import register
+from userbot import ALIVE_NAME
+from userbot import CMD_HANDLER as cmd
+from userbot import CMD_HELP, bot
+from userbot.events import man_cmd
 from userbot.utils import edit_delete, edit_or_reply
 
 
-@register(outgoing=True, pattern=r"^\.open(?: |$)(.*)")
+@bot.on(man_cmd(outgoing=True, pattern=r"open(?: |$)(.*)"))
 async def _(event):
     b = await event.client.download_media(await event.get_reply_message())
     with open(b, "r") as a:
         c = a.read()
-    a = await event.edit("**Berhasil Membaca Berkas**")
+    a = await edit_or_reply(event, "**Berhasil Membaca Berkas**")
     if len(c) > 4095:
         await a.edit("**File Terlalu Panjang Untuk dibaca**")
     else:
@@ -33,17 +36,17 @@ async def _(event):
     os.remove(b)
 
 
-@register(outgoing=True, pattern=r"^\.sendbot (.*)")
+@bot.on(man_cmd(outgoing=True, pattern=r"sendbot (.*)"))
 async def _(event):
     if event.fwd_from:
         return
     chat = str(event.pattern_match.group(1).split(" ", 1)[0])
     link = str(event.pattern_match.group(1).split(" ", 1)[1])
     if not link:
-        return await event.edit("**Maaf BOT Tidak Merespond.**")
+        return await edit_or_reply(event, "**Maaf BOT Tidak Merespond.**")
 
     botid = await event.client.get_entity(chat)
-    await event.edit("`Processing...`")
+    await edit_or_reply(event, "`Processing...`")
     async with bot.conversation(chat) as conv:
         try:
             response = conv.wait_event(
@@ -53,22 +56,20 @@ async def _(event):
             response = await response
             await bot.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
-            await event.reply(f"**Unblock Terlebih dahulu {chat} dan coba lagi.**")
+            await edit_delete(event, f"**Unblock Terlebih dahulu {chat} dan coba lagi.**", 15)
             return
         except BaseException:
-            await event.edit("**Tidak dapat menemukan bot itu 🥺**")
-            await sleep(2)
-            return await event.delete()
+            await edit_delete(event, "**Tidak dapat menemukan bot itu 🥺**", 10)
 
-        await event.edit(f"**Pesan Terkirim:** `{link}`\n**Kepada: {chat}**")
+        await edit_or_reply(event, f"**Pesan Terkirim:** `{link}`\n**Kepada: {chat}**")
         await bot.send_message(event.chat_id, response.message)
         await bot.send_read_acknowledge(event.chat_id)
         await event.client.delete_messages(conv.chat_id, [msg.id, response.id])
 
 
-@register(outgoing=True, groups_only=True, pattern=r"^\.unbanall$")
+@bot.on(man_cmd(outgoing=True, pattern=r"unbanall$"))
 async def _(event):
-    await event.edit("`Searching Participant Lists...`")
+    await edit_or_reply(event, "`Searching Participant Lists...`")
     p = 0
     title = (await event.get_chat()).title
     async for i in event.client.iter_participants(
@@ -81,10 +82,10 @@ async def _(event):
             p += 1
         except BaseException:
             pass
-    await event.edit(f"**Berhasil unbanned** `{p}` **Orang di Grup {title}**")
+    await edit_or_reply(event, f"**Berhasil unbanned** `{p}` **Orang di Grup {title}**")
 
 
-@register(outgoing=True, pattern=r"^\.(?:dm)\s?(.*)?")
+@bot.on(man_cmd(outgoing=True, pattern=r"(?:dm)\s?(.*)?"))
 async def _(event):
     p = event.pattern_match.group(1)
     m = p.split(" ")
@@ -96,18 +97,18 @@ async def _(event):
     mssg = await event.get_reply_message()
     if event.reply_to_msg_id:
         await event.client.send_message(chat_id, mssg)
-        await event.edit("**Berhasil Mengirim Pesan Anda.**")
+        await edit_or_reply(event, "**Berhasil Mengirim Pesan Anda.**")
     msg = "".join(i + " " for i in m[1:])
     if msg == "":
         return
     try:
         await event.client.send_message(chat_id, msg)
-        await event.edit("**Berhasil Mengirim Pesan Anda.**")
+        await edit_or_reply(event, "**Berhasil Mengirim Pesan Anda.**")
     except BaseException:
-        await event.edit("**ERROR: Gagal Mengirim Pesan.**")
+        await edit_delete(event, "**ERROR: Gagal Mengirim Pesan.**", 15)
 
 
-@register(outgoing=True, pattern=r"^\.fwdreply ?(.*)")
+@bot.on(man_cmd(outgoing=True, pattern=r"fwdreply ?(.*)"))
 async def _(e):
     message = e.pattern_match.group(1)
     if not e.reply_to_msg_id:
@@ -120,19 +121,19 @@ async def _(e):
     await edit_delete(e, "**Silahkan Check di Private**", 15)
 
 
-@register(outgoing=True, pattern=r"^\.getlink(?: |$)(.*)", groups_only=True)
+@bot.on(man_cmd(outgoing=True, pattern=r"getlink(?: |$)(.*)"))
 async def _(event):
-    await event.edit("`Processing...`")
+    await edit_or_reply(event, "`Processing...`")
     try:
         e = await event.client(
             ExportChatInviteRequest(event.chat_id),
         )
     except ChatAdminRequiredError:
         return await bot.send_message(f"**Maaf {ALIVE_NAME} Bukan Admin 👮**")
-    await event.edit(f"**Link Invite GC**: {e.link}")
+    await edit_or_reply(event, f"**Link Invite GC**: {e.link}")
 
 
-@register(outgoing=True, pattern=r"^\.tmsg (.*)")
+@bot.on(man_cmd(outgoing=True, pattern=r"tmsg (.*)"))
 async def _(event):
     k = await event.get_reply_message()
     if k:
@@ -144,14 +145,14 @@ async def _(event):
     if not u:
         u = "me"
     a = await bot.get_messages(event.chat_id, 0, from_user=u)
-    await event.edit(
+    await edit_or_reply(event,
         f"**Total ada `{a.total}` Chat Yang dikirim Oleh saya di Grup Chat ini**"
     )
 
 
-@register(outgoing=True, pattern=r"^\.limit(?: |$)(.*)")
+@bot.on(man_cmd(outgoing=True, pattern=r"limit(?: |$)(.*)"))
 async def _(event):
-    await event.edit("`Processing...`")
+    await edit_or_reply(event, "`Processing...`")
     async with bot.conversation("@SpamBot") as conv:
         try:
             response = conv.wait_event(
@@ -161,15 +162,77 @@ async def _(event):
             response = await response
             await bot.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
-            await event.edit("**Mohon Unblock @SpamBot dan coba lagi**")
+            await edit_or_reply(event, "**Mohon Unblock @SpamBot dan coba lagi**")
             return
-        await event.edit(f"~ {response.message.message}")
+        await edit_or_reply(event, f"~ {response.message.message}")
+
+
+@bot.on(man_cmd(outgoing=True, pattern=r"limited ?(.*)"))
+async def _(e):
+    match = e.pattern_match.group(1)
+    msg = await edit_or_reply(e, "`Processing...`")
+    if match:
+        lel = match
+    elif e.reply_to_msg_id:
+        reply = await e.get_reply_message()
+        lel = reply.sender_id
+    else:
+        lel = "me"
+    manbot = await e.client.get_entity(lel)
+    rest = manbot.restriction_reason
+    if rest:
+        xx = f"**Restriction on** {get_display_name(manbot)}\n".join(
+            f"\n• `{a}`" for a in rest
+        )
+        return await msg.edit(xx)
+    return await msg.edit(
+        "~ Kabar baik, akun Anda tidak dibatasi. Anda bebas, sebebas burung yang terbang lepas."
+    )
+
+
+@bot.on(man_cmd(outgoing=True, pattern="view"))
+async def _(event):
+    reply_message = await event.get_reply_message()
+    if not reply_message:
+        await edit_or_reply(event, "**Mohon Reply ke Link**")
+        return
+    if not reply_message.text:
+        await edit_or_reply(event, "**Mohon Reply ke Link**")
+        return
+    chat = "@chotamreaderbot"
+    xx = await edit_or_reply(event, "`Processing...`")
+    async with event.client.conversation(chat) as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=272572121)
+            )
+            await event.client.forward_messages(chat, reply_message)
+            response = await response
+            await event.client.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await xx.edit("**Silahkan unblock @chotamreaderbot dan coba lagi**")
+            return
+        if response.text.startswith(""):
+            await xx.edit("Am I Dumb Or Am I Dumb?")
+        else:
+            await xx.delete()
+            await event.client.send_message(event.chat_id, response.message)
 
 
 CMD_HELP.update(
     {
-        "open": "**Plugin : **`open`\
-        \n\n  •  **Syntax :** `.open`\
+        "view": f"**Plugin : **`view`\
+        \n\n  •  **Syntax :** `{cmd}view` <reply ke link>\
+        \n  •  **Function : **Untuk Melihat isi web dengan instan view telegraph.\
+    "
+    }
+)
+
+
+CMD_HELP.update(
+    {
+        "open": f"**Plugin : **`open`\
+        \n\n  •  **Syntax :** `{cmd}open` <reply ke file>\
         \n  •  **Function : **Untuk Melihat isi File Menjadi Text yang dikirim menjadi pesan telegram.\
     "
     }
@@ -178,10 +241,10 @@ CMD_HELP.update(
 
 CMD_HELP.update(
     {
-        "dm": "**Plugin : **`dm`\
-        \n\n  •  **Syntax :** `.dm` <username> <text>\
+        "dm": f"**Plugin : **`dm`\
+        \n\n  •  **Syntax :** `{cmd}dm` <username> <text>\
         \n  •  **Function : **Untuk mengirim chat dengan menggunakan userbot.\
-        \n\n  •  **Syntax :** `.fwdreply` <username> <text>\
+        \n\n  •  **Syntax :** `{cmd}fwdreply` <username> <text>\
         \n  •  **Function : **Untuk meneruskan chat yang di reply dengan membalasnya ke pc.\
     "
     }
@@ -190,8 +253,8 @@ CMD_HELP.update(
 
 CMD_HELP.update(
     {
-        "sendbot": "**Plugin : **`sendbot`\
-        \n\n  •  **Syntax :** `.sendbot` <username bot> <text>\
+        "sendbot": f"**Plugin : **`sendbot`\
+        \n\n  •  **Syntax :** `{cmd}sendbot` <username bot> <text>\
         \n  •  **Function : **Untuk mengirim ke bot dan mendapatkan respond chat dengan menggunakan userbot.\
     "
     }
@@ -200,8 +263,8 @@ CMD_HELP.update(
 
 CMD_HELP.update(
     {
-        "tmsg": "**Plugin : **`tmsg`\
-        \n\n  •  **Syntax :** `.tmsg` <username/me>\
+        "tmsg": f"**Plugin : **`tmsg`\
+        \n\n  •  **Syntax :** `{cmd}tmsg` <username/me>\
         \n  •  **Function : **Untuk Menghitung total jumlah chat yang sudah dikirim.\
     "
     }
@@ -210,8 +273,8 @@ CMD_HELP.update(
 
 CMD_HELP.update(
     {
-        "getlink": "**Plugin : **`getlink`\
-        \n\n  •  **Syntax :** `.getlink`\
+        "getlink": f"**Plugin : **`getlink`\
+        \n\n  •  **Syntax :** `{cmd}getlink`\
         \n  •  **Function : **Untuk Mendapatkan link invite grup chat.\
     "
     }
@@ -220,8 +283,8 @@ CMD_HELP.update(
 
 CMD_HELP.update(
     {
-        "unbanall": "**Plugin : **`unbanall`\
-        \n\n  •  **Syntax :** `.unbanall`\
+        "unbanall": f"**Plugin : **`unbanall`\
+        \n\n  •  **Syntax :** `{cmd}unbanall`\
         \n  •  **Function : **Untuk Menghapus Semua Pengguna yang dibanned di Daftar Banned GC.\
     "
     }
@@ -229,9 +292,11 @@ CMD_HELP.update(
 
 CMD_HELP.update(
     {
-        "limit": "**Plugin : **`limit`\
-        \n\n  •  **Syntax :** `.limit`\
+        "limit": f"**Plugin : **`limit`\
+        \n\n  •  **Syntax :** `{cmd}limit`\
         \n  •  **Function : **Untuk Mengecek akun anda sedang terkena limit atau tidak dengan menggunakan @spambot.\
+        \n\n  •  **Syntax :** `{cmd}limited`\
+        \n  •  **Function : **Untuk Mengecek akun anda atau orang lain sedang terkena limit atau tidak.\
     "
     }
 )
