e907c3c3ff203a7ee84136df2ba8c748cde47a37
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-10-27 23:04:21 +0700

    scrapers: ytdl: change proper regex

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -399,7 +399,7 @@ async def yt_search(video_q):
     await video_q.edit(output, link_preview=False)
 
 
-@bot.on(man_cmd(outgoing=True, pattern=r"yt(audio|video) (.*)"))
+@bot.on(man_cmd(outgoing=True, pattern=r"yt(audio|video( \d{0,4})?) (.*)"))
 async def download_video(v_url):
     """For .yt command, download media from YouTube and many other sites."""
     dl_type = v_url.pattern_match.group(1).lower()
