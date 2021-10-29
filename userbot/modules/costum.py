839f924a80798d854e7146e9caf0eb66fa128f82
mrismanaziz <mrismanaziz@gmail.com>
2021-10-29 21:57:12 +0000

    Man-Userbot: auto fixes
    Signed-off-by: mrismanaziz <mrismanaziz@gmail.com>

--- userbot/modules/costum.py
+++ userbot/modules/costum.py
@@ -15,7 +15,7 @@ from sqlalchemy.orm.exc import UnmappedInstanceError
 from userbot import BOTLOG_CHATID
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP, bot
-from userbot.events import man_cmd, register
+from userbot.events import man_cmd
 
 
 @bot.on(man_cmd(outgoing=True, pattern=r"\.(\S+)"))
