0a9bff312bf2a5fef278f6a62dc552809251d7b1
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-11-10 12:50:21 +0700

    Create utils.py

---
+++ userbot/utils/utils.py
@@ -0,0 +1,77 @@
+# Credits: @mrismanaziz
+# FROM Man-Userbot <https://github.com/mrismanaziz/Man-Userbot>
+# t.me/SharingUserbot & t.me/Lunatic0de
+
+import importlib
+import logging
+import os
+import sys
+from pathlib import Path
+
+from userbot import CMD_HELP, LOGS, bot
+
+
+def load_module(shortname):
+    if shortname.startswith("__"):
+        pass
+    elif shortname.endswith("_"):
+        path = Path(f"userbot/modules/{shortname}.py")
+        name = "userbot.modules.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(mod)
+        LOGS.info("Successfully imported " + shortname)
+    else:
+
+        path = Path(f"userbot/modules/{shortname}.py")
+        name = "userbot.modules.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        mod.bot = bot
+        mod.LOGS = LOGS
+        mod.CMD_HELP = CMD_HELP
+        mod.logger = logging.getLogger(shortname)
+        spec.loader.exec_module(mod)
+        # for imports
+        sys.modules["userbot.modules." + shortname] = mod
+        LOGS.info("Successfully imported " + shortname)
+
+
+def start_assistant(shortname):
+    if shortname.startswith("__"):
+        pass
+    elif shortname.endswith("_"):
+        path = Path(f"userbot/modules/assistant/{shortname}.py")
+        name = "userbot.modules.assistant.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(mod)
+        LOGS.info("Starting Your Assistant Bot.")
+        LOGS.info("Assistant Sucessfully imported " + shortname)
+    else:
+        path = Path(f"userbot/modules/assistant/{shortname}.py")
+        name = "userbot.modules.assistant.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        mod.tgbot = bot.tgbot
+        spec.loader.exec_module(mod)
+        sys.modules["userbot.modules.assistant" + shortname] = mod
+        LOGS.info("Successfully imported" + shortname) 
+
+
+def remove_plugin(shortname):
+    try:
+        try:
+            for i in CMD_HELP[shortname]:
+                bot.remove_event_handler(i)
+            del CMD_HELP[shortname]
+
+        except BaseException:
+            name = f"userbot.modules.{shortname}"
+
+            for i in reversed(range(len(bot._event_builders))):
+                ev, cb = bot._event_builders[i]
+                if cb.__module__ == name:
+                    del bot._event_builders[i]
+    except BaseException:
+        raise ValueError
