102df272893a456d162d272268bcfe98c54022cd
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-11-10 13:01:29 +0700

    Update utils.py

--- userbot/utils/utils.py
+++ userbot/utils/utils.py
@@ -2,12 +2,161 @@
 # FROM Man-Userbot <https://github.com/mrismanaziz/Man-Userbot>
 # t.me/SharingUserbot & t.me/Lunatic0de
 
+
+import asyncio
+import sys
+from importlib import import_module
+from pathlib import Path
+from random import randint
+
+from pytgcalls import idle
+from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
+from telethon.tl.functions.channels import InviteToChannelRequest, JoinChannelRequest
+from telethon.tl.functions.contacts import UnblockRequest
+
 import importlib
 import logging
 import sys
 from pathlib import Path
 
 from userbot import CMD_HELP, LOGS, bot
+from userbot import ALIVE_NAME, BOT_TOKEN, BOT_VER, BOTLOG_CHATID
+from userbot import CMD_HANDLER as cmd
+from userbot import LOGS, UPSTREAM_REPO_BRANCH, bot, call_py
+from userbot.modules import ALL_MODULES
+from userbot.modules.sql_helper.globals import addgvar, gvarstatus
+from userbot.utils import start_assistant
+
+
+async def autobot():
+    if gvarstatus("BOT_TOKEN"):
+        return
+    if BOT_TOKEN:
+        return addgvar("BOT_TOKEN", BOT_TOKEN)
+    await bot.start()
+    await bot.send_message(
+        BOTLOG_CHATID, "**SEDANG MEMBUAT BOT TELEGRAM UNTUK ANDA DI @BotFather**"
+    )
+    who = await bot.get_me()
+    name = who.first_name + " Assistant Bot"
+    if who.username:
+        username = who.username + "_ubot"
+    else:
+        username = "man" + (str(who.id))[5:] + "ubot"
+    bf = "@BotFather"
+    await bot(UnblockRequest(bf))
+    await bot.send_message(bf, "/cancel")
+    await asyncio.sleep(1)
+    await bot.send_message(bf, "/start")
+    await asyncio.sleep(1)
+    await bot.send_message(bf, "/newbot")
+    await asyncio.sleep(1)
+    isdone = (await bot.get_messages(bf, limit=1))[0].text
+    if isdone.startswith("That I cannot do."):
+        LOGS.info(
+            "Silakan buat Bot dari @BotFather dan tambahkan tokennya di var BOT_TOKEN"
+        )
+        sys.exit(1)
+    await bot.send_message(bf, name)
+    await asyncio.sleep(1)
+    isdone = (await bot.get_messages(bf, limit=1))[0].text
+    if not isdone.startswith("Good."):
+        await bot.send_message(bf, "My Assistant Bot")
+        await asyncio.sleep(1)
+        isdone = (await bot.get_messages(bf, limit=1))[0].text
+        if not isdone.startswith("Good."):
+            LOGS.info(
+                "Silakan buat Bot dari @BotFather dan tambahkan tokennya di var BOT_TOKEN"
+            )
+            sys.exit(1)
+    await bot.send_message(bf, username)
+    await asyncio.sleep(1)
+    isdone = (await bot.get_messages(bf, limit=1))[0].text
+    await bot.send_read_acknowledge("botfather")
+    if isdone.startswith("Sorry,"):
+        ran = randint(1, 100)
+        username = "man" + (str(who.id))[6:] + str(ran) + "ubot"
+        await bot.send_message(bf, username)
+        await asyncio.sleep(1)
+        nowdone = (await bot.get_messages(bf, limit=1))[0].text
+        if nowdone.startswith("Done!"):
+            token = nowdone.split("`")[1]
+            addgvar("BOT_TOKEN", f"{token}")
+            addgvar("BOT_USERNAME", f"@{username}")
+            await bot.send_message(bf, "/setinline")
+            await asyncio.sleep(1)
+            await bot.send_message(bf, f"@{username}")
+            await asyncio.sleep(1)
+            await bot.send_message(bf, "Search")
+            await asyncio.sleep(3)
+            await bot.send_message(bf, "/setuserpic")
+            await asyncio.sleep(1)
+            await bot.send_message(bf, f"@{username}")
+            await asyncio.sleep(1)
+            await bot.send_file(bf, "userbot/resources/logo.jpg")
+            await asyncio.sleep(3)
+            await bot.send_message(bf, "/setabouttext")
+            await asyncio.sleep(1)
+            await bot.send_message(bf, f"@{username}")
+            await asyncio.sleep(1)
+            await bot.send_message(bf, f"Managed With ☕️ By {who.first_name}")
+            await asyncio.sleep(3)
+            await bot.send_message(bf, "/setdescription")
+            await asyncio.sleep(1)
+            await bot.send_message(bf, f"@{username}")
+            await asyncio.sleep(1)
+            await bot.send_message(
+                bf, f"✨ Owner ~ {who.first_name} ✨\n\n✨ Powered By ~ @Lunatic0de ✨"
+            )
+            await bot(InviteToChannelRequest(int(BOTLOG_CHATID), [username]))
+            await bot.send_message(
+                BOTLOG_CHATID,
+                f"**BERHASIL MEMBUAT BOT TELEGRAM DENGAN USERNAME @{username}**",
+            )
+        else:
+            LOGS.info(
+                "Silakan Hapus Beberapa Bot Telegram Anda di @Botfather atau Set Var BOT_TOKEN dengan token bot"
+            )
+            sys.exit(1)
+    elif isdone.startswith("Done!"):
+        token = isdone.split("`")[1]
+        addgvar("BOT_TOKEN", f"{token}")
+        addgvar("BOT_USERNAME", f"@{username}")
+        await bot.send_message(bf, "/setinline")
+        await asyncio.sleep(1)
+        await bot.send_message(bf, f"@{username}")
+        await asyncio.sleep(1)
+        await bot.send_message(bf, "Search")
+        await asyncio.sleep(3)
+        await bot.send_message(bf, "/setuserpic")
+        await asyncio.sleep(1)
+        await bot.send_message(bf, f"@{username}")
+        await asyncio.sleep(1)
+        await bot.send_file(bf, "userbot/resources/logo.jpg")
+        await asyncio.sleep(3)
+        await bot.send_message(bf, "/setabouttext")
+        await asyncio.sleep(1)
+        await bot.send_message(bf, f"@{username}")
+        await asyncio.sleep(1)
+        await bot.send_message(bf, f"Managed With ☕️ By {who.first_name}")
+        await asyncio.sleep(3)
+        await bot.send_message(bf, "/setdescription")
+        await asyncio.sleep(1)
+        await bot.send_message(bf, f"@{username}")
+        await asyncio.sleep(1)
+        await bot.send_message(
+            bf, f"✨ Owner ~ {who.first_name} ✨\n\n✨ Powered By ~ @Lunatic0de ✨"
+        )
+        await bot(InviteToChannelRequest(int(BOTLOG_CHATID), [username]))
+        await bot.send_message(
+            BOTLOG_CHATID,
+            f"**BERHASIL MEMBUAT BOT TELEGRAM DENGAN USERNAME @{username}**",
+        )
+    else:
+        LOGS.info(
+            "Silakan Hapus Beberapa Bot Telegram Anda di @Botfather atau Set Var BOT_TOKEN dengan token bot"
+        )
+        sys.exit(1)
 
 
 def load_module(shortname):
