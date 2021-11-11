d6dbacfa1f9a08a6abf1d895d36effde5f94a965
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-11-11 07:23:58 +0700

    Update stickers_v2.py

--- userbot/modules/stickers_v2.py
+++ userbot/modules/stickers_v2.py
@@ -49,49 +49,25 @@ async def _(event):
 
 @bot.on(man_cmd(outgoing=True, pattern=r"get$"))
 async def _(event):
-    if event.fwd_from:
-        return
-    if not event.reply_to_msg_id:
-        await event.edit("`Mohon Balas Ke Sticker `")
-        return
-    reply_message = await event.get_reply_message()
-    if not reply_message.media:
-        await event.edit("`Mohon Balas Ke Sticker `")
-        return
-    chat = "@stickers_to_image_bot"
-    await event.edit("`Mengubah Menjadi Gambar....`")
-    async with event.client.conversation(chat) as conv:
-        try:
-            response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=611085086)
-            )
-            msg = await event.client.forward_messages(chat, reply_message)
-            response = await response
-        except YouBlockedUserError:
-            await event.reply("Buka Blokir @stickers_to_image_bot Lalu Coba Lagi")
+        rep_msg = await event.get_reply_message()
+
+        if not event.is_reply or not rep_msg.sticker:
+            await event.edit("**Harap balas ke stiker.**")
             return
-        if response.text.startswith("I understand only stickers"):
-            await event.edit(
-                "`Maaf , Saya Tidak Bisa Mengubah Ini Menjadi Gambar, Periksa Kembali Apakah Itu Sticker Animasi?`"
-            )
-        else:
-            response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=611085086)
-            )
-            response = await response
-            if response.text.startswith("..."):
-                response = conv.wait_event(
-                    events.NewMessage(incoming=True, from_users=611085086)
-                )
-                response = await response
-                await event.delete()
-                await event.client.send_message(
-                    event.chat_id, response.message, reply_to=reply_message.id
-                )
-                await event.client.delete_message(event.chat_id, [msg.id, response.id])
-            else:
-                await event.edit("`Coba Lagi`")
-        await bot.send_read_acknowledge(conv.chat_id)
+        await event.edit("`Mengconvert ke foto...`")
+        foto = io.BytesIO()
+        foto = await event.client.download_media(rep_msg.sticker, foto)
+
+        im = Image.open(foto).convert("RGB")
+        im.save("sticker.png", "png")
+        await event.client.send_file(
+            event.chat_id,
+            "sticker.png",
+            reply_to=rep_msg,
+        )
+
+        await event.delete()
+        os.remove("sticker.png")
 
 
 @bot.on(man_cmd(outgoing=True, pattern=r"stoi$"))
