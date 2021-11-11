a5aa6219c344f3901b0bbe56bb48b7019f9506fe
sielzz <93588547+sielzz@users.noreply.github.com>
2021-11-11 21:47:25 +0700

    Create __main__.py

---
+++ userbot/__main__.py
@@ -0,0 +1,67 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
+#
+""" Userbot start point """
+
+import sys
+from importlib import import_module
+
+from pytgcalls import idle
+from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
+from telethon.tl.functions.channels import JoinChannelRequest
+
+from userbot import ALIVE_NAME, BOT_VER, BOTLOG_CHATID
+from userbot import CMD_HANDLER as cmd
+from userbot import LOGS, UPSTREAM_REPO_BRANCH, bot, call_py
+from userbot.modules import ALL_MODULES
+
+INVALID_PH = (
+    "\nERROR: Nomor Telepon yang kamu masukkan SALAH."
+    "\nTips: Gunakan Kode Negara beserta nomornya atau periksa nomor telepon Anda dan coba lagi."
+)
+
+try:
+    bot.start()
+    call_py.start()
+except PhoneNumberInvalidError:
+    print(INVALID_PH)
+    sys.exit(1)
+
+for module_name in ALL_MODULES:
+    imported_module = import_module("userbot.modules." + module_name)
+
+LOGS.info(
+    f"Jika {ALIVE_NAME} Membutuhkan Bantuan, Silahkan Gabung ke Grup https://t.me/SharingUserbot"
+)
+
+LOGS.info(f"Sielzz-UserBot ⚙️ V{BOT_VER} [🔥 AKTIF YAA NGENTOT!!! 🔥]")
+
+
+async def man_userbot_on():
+    try:
+        if BOTLOG_CHATID != 0:
+            await bot.send_message(
+                BOTLOG_CHATID,
+                f"🔥 **Sielzz-UserBot Aktif Yaa Ngentot!!!**\n━━\n➠ **Userbot Version -** `{BOT_VER}@{UPSTREAM_REPO_BRANCH}`\n➠ **Ketik** `{cmd}alive` **untuk Mengecheck Bot**\n━━",
+            )
+    except Exception as e:
+        LOGS.info(str(e))
+    # KALO LU NGEFORK LINK CH & GRUP PUNYA GUA NYA JANGAN DI HAPUS YA GOBLOK 😡
+    try:
+        await bot(JoinChannelRequest("@sielzzsupport"))
+        await bot(JoinChannelRequest("@sielzzproject"))
+    except BaseException:
+        pass
+
+
+# JANGAN DI HAPUS GOBLOK 😡 LU COPY/EDIT AJA TINGGAL TAMBAHIN PUNYA LU
+# DI HAPUS GUA GBAN YA 🥴 GUA TANDAIN LU AKUN TELENYA 😡
+bot.loop.create_task(man_userbot_on())
+
+idle()
+if len(sys.argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.run_until_disconnected()
