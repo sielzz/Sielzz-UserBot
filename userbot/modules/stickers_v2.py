5abd9b37de475a45fc220c49c1088e0e831b84ab
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-11-11 07:46:13 +0700

    Update stickers_v2.py

--- userbot/modules/stickers_v2.py
+++ userbot/modules/stickers_v2.py
@@ -70,15 +70,9 @@ async def _(event):
 
 @bot.on(man_cmd(outgoing=True, pattern=r"stoi$"))
 async def sticker_to_png(sticker):
-    if not sticker.is_reply:
-        await sticker.edit("`NULL information to feftch...`")
-        return False
-
     img = await sticker.get_reply_message()
-    if not img.document:
-        await sticker.edit("`Maaf , Ini Bukan Sticker`")
-        return False
-
+    if not img.document or not img.is_reply
+        return await sticker.edit("**Harap balas ke stiker**")
     await sticker.edit("`Berhasil Mengambil Sticker!`")
     image = io.BytesIO()
     await sticker.client.download_media(img, image)
@@ -88,4 +82,3 @@ async def sticker_to_png(sticker):
         sticker.chat_id, image, reply_to=img.id, force_document=True
     )
     await sticker.delete()
-    return
